3792e98aa2efd4c44f50f7f5d8a48f1b3baf659b
chani529 <chani529@github.com>
2023-04-16 20:50:30 +0900

    Initial Commit

---
+++ study/studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyApp.swift
+//  study
+//
+//  Created by 손찬희 on 2023/04/16.
+//
+
+import SwiftUI
+
+@main
+struct studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
