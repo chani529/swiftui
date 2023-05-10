6f1d8a8d174d79f3dd2e05aaa4aa223789950ace
chani529 <chani529@github.com>
2023-05-11 01:26:46 +0900

    [ADD] Station Marker

--- study/View/Station/StationMapView.swift
+++ study/View/Station/StationMapView.swift
@@ -9,35 +9,83 @@ import SwiftUI
 import NMapsMap
 
 struct StationMapView: View {
-    @State var coord: (Double, Double) = (126.9784147, 37.5666805)
+    @State var coord: (Double, Double) = (126.9783881, 37.5666102)
+//    @State var coord: (Double, Double) = (37.5666102, 126.9783881)
     var body: some View {
+        TabView{
         ZStack {
             VStack {
-                Button(action: {coord = (129.05562775, 35.1379222)}) {
-                    Text("Move to Busan")
+                HStack{
+                    Button(action: {
+                        // 버튼이 눌렸을 때 실행할 액션
+                    }) {
+                        Text("O")
+                            .font(.system(size: 12))
+                            .foregroundColor(.black)
+                            .frame( height: 5)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(45)
+                            .shadow(radius: 5)
+                    }
+                    Button(action: {
+                        // 버튼이 눌렸을 때 실행할 액션
+                    }) {
+                        Text("버튼 텍스트")
+                            .font(.system(size: 12))
+                            .foregroundColor(.black)
+                            .frame(width: UIScreen.main.bounds.width * 0.6, height: 5)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(25)
+                            .shadow(radius: 5)
+                    }
                 }
-                Button(action: {coord = (127.269311, 37.413294)}) {
-                    Text("Move to Seoul somewhere")
-                }
+              
                 Spacer()
+                HStack{
+                    Spacer()
+                    VStack(alignment: .trailing){
+                   
+                        Button(action: {
+                            // 버튼이 눌렸을 때 실행할 액션
+                        }) {
+                            Text("O")
+                                .font(.system(size: 12))
+                                .foregroundColor(.black)
+                                .frame( height: 5)
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(45)
+                                .shadow(radius: 5)
+                        }
+                        
+                    }
+                    .padding(30)
+                }
+                
             }
             .zIndex(10)
             
             UIMapView(coord: coord)
                 .edgesIgnoringSafeArea(.vertical)
-        }
+                
+        }// zstack
+        }// TabView
     }
 }
 
 struct UIMapView: UIViewRepresentable {
     var coord: (Double, Double)
-  
     func makeUIView(context: Context) -> NMFNaverMapView {
         let view = NMFNaverMapView()
-        view.showZoomControls = true
-        view.mapView.positionMode = .normal
-        view.mapView.zoomLevel = 17
-      
+         view.showZoomControls = true
+         view.mapView.positionMode = .normal
+         view.mapView.zoomLevel = 17
+
+         let marker = NMFMarker(position: NMGLatLng(lat: 37.5666102, lng: 126.9783881))
+         marker.iconImage = NMFOverlayImage(name: "img_marker")
+         marker.mapView = view.mapView
         return view
     }
     
@@ -47,6 +95,7 @@ struct UIMapView: UIViewRepresentable {
         cameraUpdate.animation = .fly
         cameraUpdate.animationDuration = 1
         uiView.mapView.moveCamera(cameraUpdate)}
+    
 }
 struct StataionMapView_Previews: PreviewProvider {
     static var previews: some View {
